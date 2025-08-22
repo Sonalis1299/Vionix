8309ea119cd5ef6aa58ce9d84ba3f8ada0e4b9e8
Sonali Singh <sonalisingh.singh1299@gmail.com>
2025-08-22 12:07:49 +0530

    backend setup

---
+++ vionix/app/workspace/provider.js
@@ -0,0 +1,36 @@
+"use client"
+import { api } from '../../convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
+import React, { useEffect, useState } from 'react';
+import { UserDetailContext } from '../../context/UserDetailContext';
+
+function WorkspaceProvider({ children }) {
+
+    const newUserMutation = useMutation(api.users.CreateNewUser);
+    const {user} = useUser();
+    const [userDetail, setUserDetail] = useState()
+
+    useEffect(() => {
+        user&&CreateNewUser()
+    },[user])
+
+    const CreateNewUser = async() => {
+       const result = await newUserMutation({
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        picture: user?.imageUrl
+       });
+       console.log(result);
+      setUserDetail(result);
+    }
+  return (
+    <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+    <div>
+      {children}
+    </div>
+    </UserDetailContext.Provider>
+  );
+}
+
+export default WorkspaceProvider
